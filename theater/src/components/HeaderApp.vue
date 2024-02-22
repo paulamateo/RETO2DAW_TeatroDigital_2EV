c2d601667a9cb367e4273592fd1fd2946880d2da
paula_mb <a26619@svalero.com>
2024-02-22 13:50:33 +0100

    [FEATURES/VUE.JS_STRUCTURE] Conflicto con Vuetify

--- theater/src/components/HeaderApp.vue
+++ theater/src/components/HeaderApp.vue
@@ -67,6 +67,25 @@
                 <input type="text" class="search__input " id="search" name="search" placeholder="Buscar">
             </div>
 
+            <div class="button-actions button-action--visibility">
+                <button class="button__panel-admin" id="button-actions__login">
+                    <div class="button__panel-admin__content">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+                            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
+                          </svg>
+                        <p class="button-actions__text">INICIAR SESIÓN</p>
+                    </div>
+                </button>
+                <button class="button__panel-admin" id="button-actions__panel">
+                    <div class="button__panel-admin__content">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-lock" viewBox="0 0 16 16">
+                            <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2m3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2M5 8h6a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1"/>
+                        </svg>
+                        <p class="button-actions__text">ACCEDER</p>
+                    </div>
+                </button>    
+            </div>
+
             <div class="header__popup" id="header__popup">
                 <div class="search">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="loupe" viewBox="0 0 16 16">
@@ -77,18 +96,32 @@
                 <nav class="nav-menu">
                     <ul>
                         <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+                                <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
+                            </svg>
+                            <a href="programming.html">INICIAR SESIÓN</a>
+                        </li>
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-lock" viewBox="0 0 16 16">
+                                <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2m3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2M5 8h6a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1"/>
+                            </svg>
+                            <a href="paneladmin.html">ACCEDER</a>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li>
                             <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" fill="currentColor" class="bi bi-calendar-week" viewBox="0 0 16 16">
                                 <path d="M11 6.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm-3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm-5 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5z"/>
                                 <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z"/>
                             </svg>
-                            <RouterLink to="/Programming"><a>PROGRAMACIÓN</a></RouterLink>
+                            <a href="programming.html">PROGRAMACIÓN</a>
                         </li>
                         <li>
                             <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-ticket-perforated" viewBox="0 0 16 16">
                                 <path d="M4 4.85v.9h1v-.9zm7 0v.9h1v-.9zm-7 1.8v.9h1v-.9H4m7 0v.9h1v-.9h-1m-7 1.8v.9h1v-.9zm7 0v.9h1v-.9zm-7 1.8v.9h1v-.9zm7 0v.9h1v-.9z"/>
                                 <path d="M1.5 3A1.5 1.5 0 0 0 0 4.5V6a.5.5 0 0 0 .5.5 1.5 1.5 0 1 1 0 3 .5.5 0 0 0-.5.5v1.5A1.5 1.5 0 0 0 1.5 13h13a1.5 1.5 0 0 0 1.5-1.5V10a.5.5 0 0 0-.5-.5 1.5 1.5 0 0 1 0-3A.5.5 0 0 0 16 6V4.5A1.5 1.5 0 0 0 14.5 3zM1 4.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 .5.5v1.05a2.5 2.5 0 0 0 0 4.9v1.05a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-1.05a2.5 2.5 0 0 0 0-4.9z"/>
                             </svg>
-                            <RouterLink to="/Billboard"><a>ENTRADAS</a></RouterLink>
+                            <a href="billboard.html">ENTRADAS</a>
                         </li>
                     </ul>
                 </nav>
@@ -98,6 +131,10 @@
 </template>
 
 <style scoped>
+    .button-action--visibility {
+        display: none;
+    }
+
     .header {
         background-color: #0b0b0b;
         color: white;
@@ -211,7 +248,7 @@
         }
         .search__input {
         margin: 0;
-        width: 280px;
+        width: 150px;
         }
         .search__input.scrolled {
         background-color: black;
@@ -246,6 +283,57 @@
         display: flex;
         margin: 0 50px 0 0;
         align-items: center;
+        }
+
+        .button-actions {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-weight: bold;
+        }
+
+        .button__panel-admin {
+        all: unset;
+        background-color: white;
+        color: black;
+        font-size: 10px;
+        border-radius: 10px;
+        cursor: pointer;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-right: 10px;
+        }
+
+        .button__panel-admin.scrolled {
+        background-color: black;
+        color: white;
+        }
+
+        .button__panel-admin__content {
+        margin: 7px 10px 7px 10px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        }
+
+        .button-actions__text { 
+        display: none;
+        }
+    }
+
+    @media screen and (min-width: 1300px) {
+        .button-actions__text { 
+            display: block;
+        }
+
+        .button__panel-admin svg {
+            margin-right: 8px;
+        } 
+
+        .search__input {
+        margin: 0;
+        width: 280px;
         }
     }
 </style>
