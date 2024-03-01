695d61991862a4a2f87fbaa13454ac01a75b487a
paula_mb <a26619@svalero.com>
2024-03-01 13:41:11 +0100

    [FRONT-END/LOGIN_SIGNUP] Popup para crear una cuenta

--- theater/src/components/PopUp-SignUp.vue
+++ theater/src/components/PopUp-SignUp.vue
@@ -1,29 +1,38 @@
 <script setup lang="ts">
-    import CreateUser  from '../components/PopUp-SignUp.vue'
     import { ref } from 'vue';
-    const login = ref(false);
+    const signUp = ref(false);
 </script>
 
 <template>
-    <v-btn class="button-open-panel">
-        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
-            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
-        </svg>
-        <span class="button-open-panel__text button-open-panel__text--visibility">INICIAR SESIÓN</span> 
+    <v-btn class="text-blue button__sign-up">
+        <span>Regístrate</span> 
+        <v-icon icon="mdi-chevron-right"></v-icon>
     </v-btn>
 
-    <v-dialog v-model="login" persistent activator="parent" width="400px">
+    <v-dialog v-model="signUp" persistent activator="parent" width="400px">
         <v-card>
             <v-card-text>
-                <div class="icon-close-popup" @click="login = false">
+                <div class="icon-close-popup" @click="signUp = false">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
                         <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
                     </svg>
                 </div>
 
-                <h2 class="popup-title">Iniciar sesión</h2>
+                <h2 class="popup-title">Crear cuenta</h2>
 
                 <form>
+                    <div class="panel-box">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="icon-panel" viewBox="0 0 16 16">
+                            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
+                        </svg>
+                        <input type="text" class="input-payment-panel" name="titular_input" placeholder="Nombre" require>
+                    </div>
+                    <div class="panel-box">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="icon-panel" viewBox="0 0 16 16">
+                            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
+                        </svg>
+                        <input type="text" class="input-payment-panel" name="titular_input" placeholder="Apellidos" require>
+                    </div>
                     <div class="panel-box">
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="icon-panel" viewBox="0 0 16 16"><path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z"/></svg>
                         <input type="text" class="input-payment-panel" name="titular_input" placeholder="Correo electrónico" require>
@@ -34,13 +43,25 @@
                         </svg>
                         <input type="password" class="input-payment-panel" name="titular_input" placeholder="Contraseña" required>
                     </div>
+                    <div class="panel-box">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" fill="currentColor" class="icon-panel" viewBox="0 0 16 16">
+                            <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.6 17.6 0 0 0 4.168 6.608 17.6 17.6 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.68.68 0 0 0-.58-.122l-2.19.547a1.75 1.75 0 0 1-1.657-.459L5.482 8.062a1.75 1.75 0 0 1-.46-1.657l.548-2.19a.68.68 0 0 0-.122-.58zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/>
+                        </svg>
+                        <input type="text" class="input-payment-panel" name="titular_input" placeholder="Teléfono" require>
+                    </div>
                 </form>
 
-                <v-btn class="mb-4 button-login" size="large" block>Acceder</v-btn>
+                <v-btn class="mb-4 button-login" size="large" block>Crear cuenta</v-btn>
 
-                <p class="popup__not-login">¿No tienes cuenta? 
+                <p class="popup__not-login">¿Ya tienes cuenta? 
+                    <v-btn class="text-blue button__sign-up" @click="signUp = false">
+                        <span>Inicia sesión</span> 
+                        <v-icon icon="mdi-chevron-right"></v-icon>
+                    </v-btn>
                     <CreateUser/>
                 </p>
+
+              
             </v-card-text>
         </v-card>
     </v-dialog>
@@ -48,6 +69,18 @@
 </template>
 
 <style scoped>
+.button__sign-up {
+    letter-spacing: 0;
+   
+  padding: 5px;
+  text-transform: lowercase;
+
+  box-shadow: none;
+}
+
+.button__sign-up span::first-letter {
+    text-transform: uppercase;
+}
 
 .icon-panel {
         position: absolute;
@@ -65,13 +98,6 @@
         .button-open-panel__text--visibility {
             display: none;
         }
-    }
-
-   .button-open-panel {
-        letter-spacing: 0;
-        font-size: 12px;
-        font-weight: 700;
-        width: 140px;
     }
 
     .button-open-panel svg {
